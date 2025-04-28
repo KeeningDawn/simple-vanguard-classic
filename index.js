d60d8d9d582e82721cef5bb5045426a9f05eaab2
Dawn <207631160+KeeningDawn@users.noreply.github.com>
2025-04-28 16:05:11 -0500

    Claim the weekly without flagging anticheat

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 module.exports = function SimpleVG(mod) {
-    let enabled = true,
-        pendingQuests = [];
+	let enabled = true,
+		pendingQuests = [];
 
-    mod.command.add('vg', () => {
-        enabled = !enabled;
-        mod.command.message(`Auto Vanguard is ${enabled ? 'en' : 'dis'}abled.`);
-    });
+	mod.dispatch.addDefinition('C_COMPLETE_EXTRA_EVENT', 99, []);
 
-    mod.game.on('enter_game', () => {
-        pendingQuests.length = 0;
-    });
+	mod.command.add('vg', () => {
+		enabled = !enabled;
+		mod.command.message(`Auto Vanguard is ${enabled ? 'en' : 'dis'}abled.`);
+	});
 
-    mod.game.me.on('change_zone', () => {
-        if (enabled && !mod.game.me.inBattleground && pendingQuests.length !== 0) completeQuests();
-    });
+	mod.game.on('enter_game', () => {
+		pendingQuests.length = 0;
+	});
 
-    mod.hook('S_COMPLETE_EVENT_MATCHING_QUEST', 1, event => {
-        if (!enabled) return;
-        pendingQuests.push(event.id);
-        if (!mod.game.me.inBattleground) completeQuests();
-        return false;
-    });
+	mod.game.me.on('change_zone', () => {
+		if (enabled && !mod.game.me.inBattleground && pendingQuests.length !== 0) completeQuests();
+	});
 
-    function completeQuests(){
-        while (pendingQuests.length > 0){
-            const id = pendingQuests.pop();
-            mod.send('C_COMPLETE_DAILY_EVENT', 1, { id });
-        }
-    }
-}
+	mod.hook('S_COMPLETE_EVENT_MATCHING_QUEST', 1, (event) => {
+		if (!enabled) return;
+		pendingQuests.push(event.id);
+		if (!mod.game.me.inBattleground) completeQuests();
+		return false;
+	});
+
+	function completeQuests() {
+		while (pendingQuests.length > 0) {
+			const id = pendingQuests.pop();
+			mod.send('C_COMPLETE_DAILY_EVENT', 1, { id });
+		}
+		mod.setTimeout(() => {
+			mod.send('C_COMPLETE_EXTRA_EVENT', 99, {});
+		}, 1000);
+	}
+};
